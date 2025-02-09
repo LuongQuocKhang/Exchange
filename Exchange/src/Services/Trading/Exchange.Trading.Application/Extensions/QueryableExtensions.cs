8626c60732fd94b0979a02caa9bcaa14b4fafb98
O2F-IT\luong.quockhang_amar <luong.quockhang@amaris.com>
2025-02-09 16:39:13 +0700

    Fix build --global

---
+++ Exchange/src/Services/Trading/Exchange.Trading.Application/Extensions/QueryableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Exchange.Trading.Application.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<List<T>> ToPaginatedListAsync<T>(
+        this IQueryable<T> query,
+        int pageIndex,
+        int pageSize,
+        Expression<Func<T, object>>? orderByDescending = null,
+        Expression<Func<T, object>>? orderByAscending = null
+        )
+    {
+        if (orderByDescending != null)
+        {
+            query = query
+                .OrderByDescending(orderByDescending);
+        }
+
+        if (orderByAscending != null)
+        {
+            query = query
+                .OrderBy(orderByAscending);
+        }
+
+        return await query
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
+}
